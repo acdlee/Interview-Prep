d8c8e7a0fed68bfda11476ce818c7d9a2ac34619
Christopher Lee <clee651@terpmail.umd.edu>
2021-04-22 17:47:31 -0400

    Finished writing the code for extended_euclid - needs commenting; added a testing file - needs more tests

--- fundamental_algorithms/extended_euclidean/extended_euclid.py
+++ fundamental_algorithms/extended_euclidean/extended_euclid.py
@@ -1,9 +1,23 @@
-#append the path with the gcd algorithm
-import sys
-sys.path.append('../gcd/')
-from gcd import gcd
+def extended_euclidean_algorithm(a, b):
+	"""
+		Extended Euclidean Algorithm:
+		computes the gcd of two inputs, along with the coefficients x, y 
+		of Bezout's identity: 
+			ax + by = gcd(a, b).
+	"""
+	(old_r, r) = (a, b)
+	(old_s, s) = (1, 0)
+	(old_t, t) = (0, 1)
+
+	while r:
+		quotient = old_r // r
+		(old_r, r) = (r, old_r - quotient * r)
+		(old_s, s) = (s, old_s - quotient * s)
+		(old_t, t) = (t, old_t - quotient * t)
 
 
+	print("coefficients: (" + str(old_s) + ", " + str(old_t) + ")" )
+	print("gcd: " + str(old_r))
 
-def extended_euclidean_algorithm():
-	pass
+
+extended_euclidean_algorithm(412, 260)
---
+++ fundamental_algorithms/extended_euclidean/test_extended_euclid.py
@@ -0,0 +1,16 @@
+import unittest
+from extended_euclid import extended_euclidean_algorithm as eea
+
+
+class TestExtendedEuclidean(unittest.TestCase):
+	"""
+		Test the extended_euclidean_algorithm(a, b)
+		from extended_euclid.py.
+	"""
+
+	#setup function for test cases
+	def setUp(self):
+		pass
+
+if __name__ == '__main__':
+	unittest.main()
