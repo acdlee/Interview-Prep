26a7a4bfad53cb258e18cad912d0f373fb8a9bce
Christopher Lee <clee651@terpmail.umd.edu>
2021-05-28 11:53:30 -0400

    added sub_prod_sum; linear t/s

--- leetcode_problems/easy/sub_prod_sum.py
+++ leetcode_problems/easy/sub_prod_sum.py
@@ -1,2 +1,18 @@
+# O(n) time and space
 def subtractProductAndSum(n):
-	
+	result = 0
+	if n:
+		product_n = 1
+		sum_n = 0
+		s = str(n)
+		for c in s:
+			i = int(c)
+			product_n *= i
+			sum_n += i
+
+		result = product_n - sum_n
+
+	return result
+
+n = 4421
+print(subtractProductAndSum(n))
