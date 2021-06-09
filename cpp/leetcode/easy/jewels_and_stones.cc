fc32851e22ad903ee95e2e39950aa7bb32a9d820
Christopher Lee <clee651@terpmail.umd.edu>
2021-06-09 12:22:07 -0400

    added c++ dir; jewelsnstones c++

---
+++ cpp/leetcode/easy/jewels_and_stones.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+//declaration 
+int numJewelsInStones(string, string);
+
+int main() {
+	string jewels = "aA";
+	string stones = "aAAbbbb";
+
+	cout << numJewelsInStones(jewels, stones) << endl;
+
+	return 0;
+}
+
+
+int numJewelsInStones(string jewels, string stones) {
+	map<char, int> m;
+	int count = 0;
+
+	for (auto c : stones) {
+		m[c]++;
+	}
+
+	//let's now count
+	for (auto c : jewels) {
+		if (m.count(c)) {
+			count += m[c];
+		}
+	}
+
+	return count;
+}
